1be4491e00bb8145c2a46a7d7c2ff75e117528e4
Ali Manian <alimanian2020@gmail.com>
2022-04-12 13:14:34 +0430

    add: Livewire Auth Component
    livewire Register, Login, Forget component and verify view added

---
+++ resources/views/livewire/auth/register.blade.php
@@ -0,0 +1,33 @@
+<main class="flex-1 flex flex-col px-4">
+    <header class="flex-none">
+        <x-title type="auth" :title="__('heading.Register')"
+                 :description="__('text.Enter your account information:')"
+                 class="flex flex-col items-center mb-3" />
+    </header>
+    <main class="flex-1 flex items-center">
+        <x-form wire:submit.prevent="register" class="w-full">
+            <section class="flex flex-col sm:flex-row sm:space-x-reverse sm:space-x-3">
+                <x-input :name="$prefix.'first_name'" type="text" maxlength="40" autofocus>
+                    {{ __('label.first_name') }}
+                </x-input>
+                <x-input :name="$prefix.'last_name'" type="text" maxlength="60">
+                    {{ __('label.last_name') }}
+                </x-input>
+            </section>
+            <x-input class="ltr text-left" :name="$prefix.'phone_number'" type="tel" maxlength="11">
+                {{ __('label.phone_number') }}
+            </x-input>
+            <x-input class="ltr text-left" :name="$prefix.'password'" type="password">
+                {{ __('label.password') }}
+            </x-input>
+            <p class="text-slate-400 body-2 text-center mb-3">
+                {!! __('text.Signing up in MyShortener means accepting the rules.', ['link' => '/', 'class' => 'font-bold']) !!}
+            </p>
+        </x-form>
+    </main>
+    <footer class="flex-none">
+        <x-button class="form-button-primary !mb-4" type="submit" form="register">
+            {{ __('button.Send verification code') }}
+        </x-button>
+    </footer>
+</main>
---
+++ resources/views/livewire/auth/verify.blade.php
@@ -0,0 +1,19 @@
+<main class="flex-1 flex flex-col px-4">
+    <header class="flex-none">
+        <x-title type="auth" :title="__('heading.Validation')"
+                 :description="__('text.Enter the verification code sent:')"
+                 class="flex flex-col items-center mb-3" />
+    </header>
+    <main class="flex-1 flex items-center">
+        <x-form wire:submit.prevent="verify" class="w-full">
+            <x-input class="ltr text-left" :name="$prefix.'verify_code'" type="tel" maxlength="6" autofocus>
+                {{ __('label.verify_code') }}
+            </x-input>
+        </x-form>
+    </main>
+    <footer class="flex-none">
+        <x-button class="form-button-primary !mb-4" type="submit" form="verify">
+            {{ __('button.Check verification code') }}
+        </x-button>
+    </footer>
+</main>
